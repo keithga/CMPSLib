f20edf347ec489c2d531813324e803e5aeb557d7
Stephen Owen <sred13@gmail.com>
2018-03-01 12:34:39 -0500

    Update Remove-CMResourceFromCollection.ps1
    Modified all [wmi] accelerator usage to *-WmiObject calls, to allow for using alternate credentials.  
    
    Added new switch -EchoCollection, which will display collection rule count before and after operations by returning a PSCustomObject containing the old and new rule count.  
    
    Changed to use CollectionID in lieu of CollectionName
    
    Adding additional logging with -Verbose

--- PSCMLib/Collections/Remove-CMResourceFromCollection.ps1
+++ PSCMLib/Collections/Remove-CMResourceFromCollection.ps1
@@ -1,24 +1,77 @@
+<#
+.Synopsis
+   Removes many members from a CM Collection with only one call
+.DESCRIPTION
+   Uses WMI to Remove many Direct Membership rules from a SCCM Device Collection with just one DB action
+.EXAMPLE
+   Remove-CMDeviceFromCollection -CollectionID XXX00750 -SiteCode XXX -System $ReadyForMigrationDevices[50..70] -EchoCollectionCount 
 
-Function Remove-CMResourceFromCollection {
-    <#
+   VERBOSE: Attempting to connect over WMI to FOXSCCM01.foxdeploy.local:\\root\SMS\Site_XXX using FoxDeploy\Admin01 credential
+VERBOSE: Resolved Collection PreAssessment_Scan with root\SMS\Site_XXX
+VERBOSE: Retrieving Class Object...
+VERBOSE: XXX00750 direct membership rule count: 22
+VERBOSE: Removing 21 rules from Collection: XXX00750
+VERBOSE: XXX00750 direct membership rule count: 1
 
-    https://keithga.wordpress.com/2018/01/25/a-replacement-for-sccm-add-cmdevicecollectiondirectmembershiprule-powershell-cmdlet/
+OldCount NewCount
+-------- --------
+      22        1
+.NOTES
+   Author Keith Garner
+          Stephen Owen
 
-    #>
-
+    URL   https://keithga.wordpress.com/2018/01/25/a-replacement-for-sccm-add-cmdevicecollectiondirectmembershiprule-powershell-cmdlet/
+.PARAMETER CollectionID
+    The SMS Collection ID to which the devices should be added
+.PARAMETER SITECODE
+    The SMS_XXX SiteCode of the CM Instance
+.PARAMETER SYSTEM
+    An array of SMS_Resource Objects to add to the collection (must contain both a devicename and ResourceID property)
+.PARAMETER EchoCollectionCount    
+    When this switch is present detailed collection count information will be displayed before and after operations
+.PARAMETER Credential
+    Provide this credential to use an alternate credential for the WMI Operations
+#>
+Function Remove-CMDeviceFromCollection {
     [CmdLetBinding()]
     Param(
-        [string]   $CollectionName,
+        [string]   $CollectionID,
+        [string]   $SiteCode,
         [parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        $System
+        $System,
+        [pscredential]$Credential,
+        [Switch]$EchoCollectionCount
     )
 
     begin {
-        $WmiArgs = Get-CMSiteForWMI
-        $CollectionQuery = Get-WmiObject @WMIArgs -Class SMS_Collection -Filter "Name = '$CollectionName' and CollectionType='2'"
-        $InParams = $CollectionQuery.PSBase.GetMethodParameters('DeleteMembershipRules')
-        $Cls = [WMIClass]"\\$($WMIArgs.ComputerName)\$($WmiArgs.NameSpace):SMS_CollectionRuleDirect"
-        $Rules = @()
+        if ($Credential){
+            $WmiArgs = @{ NameSpace = "root\SMS\Site_$SiteCode"; ComputerName = $CMDBServer; Credential = $Credential }
+            Write-Verbose "Will be using $($Credential.UserName) for this connection"
+        }
+        else {
+            $WmiArgs = @{ NameSpace = "root\SMS\Site_$SiteCode"; ComputerName = $CMDBServer}
+        }
+
+        Write-Verbose "Attempting to connect over WMI to $($WmiArgs.ComputerName):\\$($WmiArgs.NameSpace)"
+        $CollectionCount = [pscustomObject]@{'OldCount'=$null;'NewCount'=$null}
+        $CollectionQuery = Get-WmiObject @WMIArgs -Class SMS_Collection -Filter "CollectionID = '$CollectionID' and CollectionType='2'" -ErrorAction Stop 
+        if ($CollectionQuery){
+            Write-Verbose "Resolved Collection $($CollectionQuery.Name) with $($WmiArgs.NameSpace)"
+            $InParams = $CollectionQuery.PSBase.GetMethodParameters('AddMembershipRules')
+            Write-Verbose "Retrieving Class Object..."
+            $cls = Get-WmiObject @WMIArgs -Class SMS_CollectionRuleDirect -list -ErrorAction Stop       
+            $Rules = @()
+        }
+        else{
+            throw
+        }
+
+        if ($EchoCollectionCount){
+            $MemberCount = Get-WmiObject @WMIArgs -class 'SMS_Collection' -Filter "CollectionID='$CollectionID'" 
+            $MemberCount.Get()
+            $CollectionCount.OldCount = $MemberCount.CollectionRules.Count
+            Write-Verbose "$CollectionID direct membership rule count: $($CollectionCount.OldCount)"
+        }
     }
     process {
         foreach ( $sys in $System ) {
@@ -28,10 +81,19 @@ Function Remove-CMResourceFromCollection {
             $NewRule.Rulename = $sys.Name
             $Rules += $NewRule.psobject.BaseObject 
         }
+        Write-Verbose "Removing $($Rules.Count) rules from Collection: $CollectionID"
     }
     end {
         $InParams.CollectionRules += $Rules.psobject.BaseOBject
         $CollectionQuery.PSBase.InvokeMethod('DeleteMembershipRules',$InParams,$null) | Out-null
         $CollectionQuery.RequestRefresh() | out-null
+    
+        if ($EchoCollectionCount){
+            $MemberCount = Get-WmiObject @WMIArgs -class 'SMS_Collection' -Filter "CollectionID='$CollectionID'" 
+            $MemberCount.Get()
+            $CollectionCount.NewCount = $MemberCount.CollectionRules.Count
+            Write-Verbose "$CollectionID direct membership rule count: $($CollectionCount.NewCount)"
+            return $CollectionCount
+        }
     }
 }
