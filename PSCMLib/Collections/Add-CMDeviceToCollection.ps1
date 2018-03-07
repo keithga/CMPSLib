e9e73c3891848ef1b347aca71e3090278e1b2cb2
unknown <1RedOne>
2018-03-07 11:53:20 -0800

    Removed all hard coded values, moved into variables

--- PSCMLib/Collections/Add-CMDeviceToCollection.ps1
+++ PSCMLib/Collections/Add-CMDeviceToCollection.ps1
@@ -20,34 +20,74 @@
     An array of SMS_Resource Objects to add to the collection (must contain both a devicename and ResourceID property)
 .PARAMETER EchoCollectionCount    
     When this switch is present detailed collection count information will be displayed before and after operations
+.PARAMETER ComputerName
+    This command supports operating against remote computers.  Provide the computer name of the SCCM Site Server with this param.
 .PARAMETER Credential
     Provide this credential to use an alternate credential for the WMI Operations
 #>
 Function Add-CMDeviceToCollection {
     [CmdLetBinding()]
     Param(
-        [string]   $CollectionID,
-        [string]   $SiteCode,
-        [parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        $System,
+        
+        [Parameter(Mandatory=$true, 
+                   Position=0,
+                   ParameterSetName='CollNameSet')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Name')] 
+        [string]    $CollectionName,
+        [Parameter(Mandatory=$true, 
+                   Position=0,
+                   ParameterSetName='CollIDSet')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ID')] 
+        [string]    $CollectionID,
+        [parameter(Mandatory=$true,
+                   ParameterSetName = 'CollNameSet')]
+        [parameter(ParameterSetName = 'CollIDSet')]
+        [string]    $SiteCode,
+        [parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ParameterSetName = 'CollNameSet')]
+        [parameter(ParameterSetName = 'CollIDSet')]
+        [string[]]  $System,
         [pscredential]$Credential,
-        [switch]$EchoCollectionCount
+        [parameter(Mandatory=$false, 
+                   ParameterSetName = 'CollNameSet')]
+        [parameter(ParameterSetName = 'CollIDSet')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('SMSSiteServerName')] 
+        [string]    $ComputerName, 
+        [switch]    $EchoCollectionCount
     )
 
     begin {               
-        if ($Credential){
-            $WmiArgs = @{ NameSpace = "root\SMS\Site_$SiteCode"; ComputerName = $CMDBServer; Credential = $Credential }
-            Write-Verbose "Will be using $($Credential.UserName) for this connection"
-        }
-        else {
-            $WmiArgs = @{ NameSpace = "root\SMS\Site_$SiteCode"; ComputerName = $CMDBServer}
+        $WmiArgs = @{ NameSpace = "root\SMS\Site_$SiteCode"}
+        $CollectionCount = [pscustomObject]@{'OldCount'a=$null;'NewCount'=$null}
+
+        if ($PSBoundParameters.ContainsKey('CollectionID')){
+            $WMIArgs.Add('Filter', "CollectionID = '$CollectionID' and CollectionType='2'")
         }
         
-        Write-Verbose "Attempting to connect over WMI to $($WmiArgs.ComputerName):\\$($WmiArgs.NameSpace)"
-        $CollectionCount = [pscustomObject]@{'OldCount'=$null;'NewCount'=$null}
-        $CollectionQuery = Get-WmiObject @WMIArgs -Class SMS_Collection -Filter "CollectionID = '$CollectionID' and CollectionType='2'" -ErrorAction Stop 
-        if ($CollectionQuery){
-            Write-Verbose "Resolved Collection $($CollectionQuery.Name) with $($WmiArgs.NameSpace)"
+        if ($PSBoundParameters.ContainsKey('CollectionName')){{
+            $WMIArgs.Add('Filter', "CollectionName = '$CollectionName' and CollectionType='2'")
+        }
+        
+        if ($PSBoundParameters.ContainsKey('ComputerName')){
+            $WmiArgs.Add('ComputerName', $ComputerName)
+        }
+
+        if ($PSBoundParameters.ContainsKey('Credential')){
+            $WmiArgs.Add(Credential, $Credential)
+            Write-Verbose "Will be using $($Credential.UserName) for this connection"
+        }
+        write-debug "test param build out here"
+        
+        $CollectionRef = Get-WmiObject @WMIArgs -Class SMS_Collection -Filter $WMIFilter -ErrorAction Stop 
+        if ($CollectionRef){
+            Write-Verbose "Resolved Collection $($CollectionRef.Name) with $($WmiArgs.NameSpace)"
             $InParams = $CollectionQuery.PSBase.GetMethodParameters('AddMembershipRules')
             Write-Verbose "Retrieving Class Object..."
             $cls = Get-WmiObject @WMIArgs -Class SMS_CollectionRuleDirect -list -ErrorAction Stop       
@@ -58,7 +98,7 @@ Function Add-CMDeviceToCollection {
         }
         
         if ($EchoCollectionCount){
-            $MemberCount = Get-WmiObject @WMIArgs -class 'SMS_Collection' -Filter "CollectionID='$CollectionID'"
+            $MemberCount = Get-WmiObject @WMIArgs -class 'SMS_Collection' -Filter "CollectionID='$($CollectionRef.CollectionID)'"
             $MemberCount.Get()
             $CollectionCount.OldCount = $MemberCount.CollectionRules.Count
             Write-Verbose "$CollectionID direct membership rule count: $($CollectionCount.OldCount)"
@@ -72,15 +112,15 @@ Function Add-CMDeviceToCollection {
             $NewRule.Rulename = $sys.Name
             $Rules += $NewRule.psobject.BaseObject 
         }
-        Write-Verbose "Adding $($Rules.Count) rules to Collection: $CollectionID"
+        Write-Verbose "Adding $($Rules.Count) rules to Collection: $($CollectionID,$CollectionName)"
     }
     end {
         $InParams.CollectionRules += $Rules.psobject.BaseOBject
-        $CollectionQuery.PSBase.InvokeMethod('AddMembershipRules',$InParams,$null) | Out-null
-        $CollectionQuery.RequestRefresh() | out-null
+        $CollectionRef.PSBase.InvokeMethod('AddMembershipRules',$InParams,$null) | Out-null
+        $CollectionRef.RequestRefresh() | out-null
 
         if ($EchoCollectionCount){
-            $MemberCount = Get-WmiObject @WMIArgs -class 'SMS_Collection' -Filter "CollectionID='$CollectionID'" 
+            $MemberCount = Get-WmiObject @WMIArgs -class 'SMS_Collection' -Filter "CollectionID='$($CollectionRef.CollectionID)'"
             $MemberCount.Get()
             $CollectionCount.NewCount = $MemberCount.CollectionRules.Count
             Write-Verbose "$CollectionID direct membership rule count: $($CollectionCount.NewCount)"
