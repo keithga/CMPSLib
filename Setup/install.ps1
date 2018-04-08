98f328df17122a8199cd73fa7e674b06f738d863
Keith Garner <keith.garner@hotmail.com>
2018-04-07 17:02:18 -0700

    transient issue with release folder

--- Setup/install.ps1
+++ Setup/install.ps1
@@ -1,19 +1,23 @@
 [cmdletbinding()]
 param(
     $TargetZipFile = "$env:temp\CMPSLib.zip",
-    $SrcZipFile = 'https://github.com/keithga/CMPSLib/archive/master.zip'
+    $SrcZipFile = 'https://github.com/keithga/CMPSLib/archive/master.zip',
+    $Target
 )
 
 #region Get wizard bits
 
-if ( -not ( Test-Path "$PSscriptRoot\release" ) ) {
+if ( -not $Target ) { $Target = "$PSscriptRoot\Release" }
+
+
+if ( -not ( Test-Path $Target ) ) {
 
     remove-item -Force $TargetZipFile -ErrorAction SilentlyContinue | Out-Null
 
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     iwr -Uri $SrcZipFile -OutFile $TargetZipFile
 
-    Expand-Archive -Path $TargetZipFile -DestinationPath "$PSscriptRoot\release" -Force
+    Expand-Archive -Path $TargetZipFile -DestinationPath $Target -Force
 }
 
 #endregion
